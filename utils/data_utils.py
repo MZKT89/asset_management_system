483c302e963f50eb7d54b5a63132d136689f357e
MZKT89 <mzhu2587@163.com>
2025-04-13 18:21:24 +0800

    数据库更新

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -16,6 +16,7 @@ def create_connection():
     """
     try:
         conn = sqlite3.connect('asset_management.db')
+        # conn = sqlite3.connect('../asset_management.db')
         return conn
     except sqlite3.Error as e:
         print(e)
